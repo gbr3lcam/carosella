662e15b6883b47a2ae472df317642885d28e608f
Andre Luis <podolskujeromildo@gmail.com>
2024-09-19 22:55:53 -0300

    Ajustando conexao com o banco de dados

---
+++ Restaurantemain/src/main/java/com/mycompany/restaurantemain/Restaurantemain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantemain;
+
+/**
+ *
+ * @author usuario
+ */
+public class Restaurantemain {
+
+    public static void main(String[] args) {
+        
+    }
+}
